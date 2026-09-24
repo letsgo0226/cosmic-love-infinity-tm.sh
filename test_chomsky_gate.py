c568270a8962dc9c53b2ceb0b7bfa80630a8569c
letsgo0226 <97732754+letsgo0226@users.noreply.github.com>
2026-09-25 01:49:54 +0800

    Test Cosmic-Love model continuation certificate

--- test_chomsky_gate.py
+++ test_chomsky_gate.py
@@ -4,6 +4,8 @@ import subprocess
 import tempfile
 import unittest
 
+from cosmic_love_model_certificate import certificate
+
 
 class ChomskyGateTests(unittest.TestCase):
     def run_gate(self, state, command):
@@ -21,10 +23,33 @@ class ChomskyGateTests(unittest.TestCase):
             self.assertEqual(step['terminal'], 'STEP')
             self.assertEqual(step['H_struct'], 0)
             self.assertTrue(step['zero_ambiguity'])
+            self.assertTrue(step['C_direction'])
+            self.assertTrue(step['C_CL'])
+            self.assertEqual(step['model_transition_certainty'], 1)
+            self.assertIsNone(step['P_real_world'])
+
             rewind = self.run_gate(state, 'rewind')
             self.assertEqual(rewind['terminal'], 'REWIND')
             self.assertEqual(rewind['H_struct'], 0)
             self.assertTrue(rewind['zero_ambiguity'])
+            self.assertTrue(rewind['C_direction'])
+            self.assertTrue(rewind['C_CL'])
+            self.assertEqual(rewind['model_transition_certainty'], 1)
+            self.assertIsNone(rewind['P_real_world'])
+
+    def test_direction_mismatch_fails_certificate(self):
+        out = {'CF': True, 'CR': True, 'CG': True, 'CE': True, 'rev': False}
+        cert = certificate(out, 'rewind')
+        self.assertFalse(cert['C_direction'])
+        self.assertFalse(cert['C_CL'])
+        self.assertIsNone(cert['model_transition_certainty'])
+        self.assertIsNone(cert['P_real_world'])
+
+    def test_failed_invariant_fails_certificate(self):
+        out = {'CF': True, 'CR': True, 'CG': False, 'CE': True, 'rev': False}
+        cert = certificate(out, 'step')
+        self.assertFalse(cert['C_CL'])
+        self.assertIsNone(cert['H_struct'])
 
     def test_unknown_terminal_is_rejected(self):
         with tempfile.TemporaryDirectory() as tmp:
