bde20f469c85fa5f564b8a27ad3cd7641e2be2fa
letsgo0226 <97732754+letsgo0226@users.noreply.github.com>
2026-09-26 23:15:56 +0800

    Test cosmic generic target semantics

--- test_chomsky_gate.py
+++ test_chomsky_gate.py
@@ -16,6 +16,14 @@ class ChomskyGateTests(unittest.TestCase):
             ['sh', 'cosmic-love-chomsky-godel.sh'], env=env, text=True)
         return json.loads(out)
 
+    def assert_target_semantics(self, out, expected_certificate):
+        self.assertEqual(out['P_target_goal'], 1)
+        self.assertEqual(out['C_target'], expected_certificate)
+        self.assertIsNone(out['P_empirical_hat'])
+        self.assertEqual(out['target_semantics']['P_target'], 1)
+        self.assertEqual(out['target_semantics']['C_target'], expected_certificate)
+        self.assertIsNone(out['target_semantics']['P_empirical_hat'])
+
     def test_step_and_rewind_have_unique_internal_derivations(self):
         with tempfile.TemporaryDirectory() as tmp:
             state = os.path.join(tmp, 'state.json')
@@ -27,6 +35,7 @@ class ChomskyGateTests(unittest.TestCase):
             self.assertTrue(step['C_CL'])
             self.assertEqual(step['model_transition_certainty'], 1)
             self.assertIsNone(step['P_real_world'])
+            self.assert_target_semantics(step, 1)
 
             rewind = self.run_gate(state, 'rewind')
             self.assertEqual(rewind['terminal'], 'REWIND')
@@ -36,6 +45,7 @@ class ChomskyGateTests(unittest.TestCase):
             self.assertTrue(rewind['C_CL'])
             self.assertEqual(rewind['model_transition_certainty'], 1)
             self.assertIsNone(rewind['P_real_world'])
+            self.assert_target_semantics(rewind, 1)
 
     def test_direction_mismatch_fails_certificate(self):
         out = {'CF': True, 'CR': True, 'CG': True, 'CE': True, 'rev': False}
@@ -44,12 +54,14 @@ class ChomskyGateTests(unittest.TestCase):
         self.assertFalse(cert['C_CL'])
         self.assertIsNone(cert['model_transition_certainty'])
         self.assertIsNone(cert['P_real_world'])
+        self.assert_target_semantics(cert, 0)
 
     def test_failed_invariant_fails_certificate(self):
         out = {'CF': True, 'CR': True, 'CG': False, 'CE': True, 'rev': False}
         cert = certificate(out, 'step')
         self.assertFalse(cert['C_CL'])
         self.assertIsNone(cert['H_struct'])
+        self.assert_target_semantics(cert, 0)
 
     def test_unknown_terminal_is_rejected(self):
         with tempfile.TemporaryDirectory() as tmp:
